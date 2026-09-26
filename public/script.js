ffc9fee7fd39ee25f49309779dd2590dcb5b280b
Akash Polous <akashpolus123@gmail.com>
2026-09-26 20:50:33 +0500

    Update slots to 30 min intervals

--- public/script.js
+++ public/script.js
@@ -210,7 +210,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Dr. Shakil specific custom slots (Tuesday & Saturday: 11:00 AM - 12:30 PM & 01:00 PM - 06:00 PM, Break: 12:30 PM - 01:00 PM)
 const DR_SHAKIL_SLOTS = [
-  "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM",
+  "11:00 AM", "11:30 AM", "12:00 PM",
   "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM", "03:00 PM", "03:30 PM", 
   "04:00 PM", "04:30 PM", "05:00 PM", "05:30 PM"
 ];
